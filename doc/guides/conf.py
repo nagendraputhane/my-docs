abfc0657bc4905ab71864b8009dc0dbb9aa63ff8
Nagendra T P <nagendraputhane@gmail.com>
2025-05-12 10:43:16 +0530

    include rtd in conf

--- doc/guides/conf.py
+++ doc/guides/conf.py
@@ -1,10 +1,4 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sphinx_rtd_theme
 
 project = 'my-docs'
 copyright = '2025, nagendra'
@@ -13,20 +7,13 @@ html_logo = 'logo/logo.jpg'
 release = '0.0.1'
 master_doc = 'index'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = []
 
 #templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
 
 html_css_files = [
