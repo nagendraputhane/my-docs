a363ee810b9b9462e0472191497cc38a3635a637
Nagendra Puthane <nagendraputhane@gmail.com>
2025-05-14 00:01:44 +0530

    from other system

--- doc/guides/conf.py
+++ doc/guides/conf.py
@@ -7,13 +7,15 @@ html_logo = 'logo/logo.jpg'
 release = '0.0.1'
 master_doc = 'index'
 
-extensions = []
+extensions = [
+    "sphinx_rtd_theme",
+]
 
 #templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "sphinx_rtd_theme"
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
 
 html_css_files = [
